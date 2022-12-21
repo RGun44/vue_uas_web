681e8e3654a8643ca4e6167587e932dc40a436d2
RGun44* <ricogunawan444@gmail.com*>
2022-12-21 21:10:19 +0700

    Benerin Register

--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -1,26 +1,41 @@
 <template>
     <div class="container">
-        <form @submit.prevent="">
+        <form @submit.prevent="register">
             <h2 class="mb-3">Register</h2>
-            <div class="input">
-                <label for="username">Username</label>
-                <input class="form-control" type="text" name="username" placeholder="username123" />
+            <div class="form-group mb-3">
+                <label class="form-label">
+                    Username
+                </label>
+                <input type="text" class="form-control" v-model="user.username" placeholder="Username">
             </div>
-            <div class="input">
-                <label for="password">Password</label>
-                <input class="form-control" type="password" name="password" placeholder="password123" />
+            <div class="form-group mb-3">
+                <label class="form-label">
+                    Password
+                </label>
+                <input type="password" class="form-control" v-model="user.password" placeholder="Password">
             </div>
-            <div class="input">
-                <label for="name">Name</label>
-                <input class="form-control" type="text" name="name" placeholder="John Smith" />
+            <div class="form-group mb-3">
+                <label class="form-label">
+                    Name
+                </label>
+                <input type="text" class="form-control" v-model="user.name" placeholder="Username">
             </div>
-            <div class="input">
-                <label for="email">Email address</label>
-                <input class="form-control" type="text" name="email" placeholder="email@adress.com" />
+            <div class="form-group mb-3">
+                <label class="form-label">
+                    Email
+                </label>
+                <input type="text" class="form-control" v-model="user.email" placeholder="Email @">
             </div>
-            <div class="input">
-                <label for="photo">Photo</label>
-                <input class="form-control" type="file" name="photo" />
+            <div class="form-group mb-3">
+                <label class="form-label">
+                    Photo
+                </label>
+                <input class="form-control" type="file" @change="onFile" />
+                <div v-if="validation.foto" class="mt-2 alert alert-danger">
+                    {{
+                    validation.foto[0]
+                    }}
+                </div>
             </div>
             <router-link :to="{ name: 'loginPage' }" class="btn btn-primary">Register</router-link>
         </form>
@@ -28,7 +43,70 @@
 </template>
 
 <script>
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
+
 export default {
 
-};
+    setup() {
+        // state band
+        const user = reactive({
+            username: '',
+            password: '',
+            name: '',
+            email: '',
+            foto: '',
+        })
+
+        // state validation
+        const validation = ref([])
+        // vue router
+        const router = useRouter()
+
+        // method register
+        function register() {
+            const username = user.username
+            const password = user.password
+            const name = user.name
+            const email = user.email
+            const foto = user.foto
+
+            axios.post('http://localhost:8000/api/register', {
+                username: username,
+                password: password,
+                name: name,
+                email: email,
+                foto: foto,
+            }).then(() => {
+                // redirect ke post index
+                router.push({
+                    name: 'loginPage'
+                })
+            }).catch(error => {
+                // assign state validation with error
+                validation.value = error.response.data
+            })
+        }
+
+        // return
+        return {
+            user,
+            validation,
+            router,
+            register,
+        }
+    },
+
+    methods: {
+        onFile(e) {
+            const files = e.target.files
+            if (!files.length) return
+
+            const reader = new FileReader()
+            reader.readAsDataURL(files[0])
+            reader.onload = () => (this.user.foto = reader.result)
+        }
+    }
+}
 </script>
